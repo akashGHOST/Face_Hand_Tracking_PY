496ab3deb1f1a95bce04068bba3e98a461b49e41
akashGHOST <78381341+akashGHOST@users.noreply.github.com>
2021-05-09 03:12:09 +0530

    Add files via upload

---
+++ Attendence_HandTrackProject.py
@@ -0,0 +1,139 @@
+import face_recognition
+import os
+from datetime import datetime
+import cv2
+import time
+import numpy as np
+import HandTrackinModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.5)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+
+volRange = volume.GetVolumeRange()
+
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+
+path = 'AttendenceImg'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendence(name):
+    with open('Attendence.csv','r+')as f:
+        myDataList= f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(', ')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {dtString}')
+
+
+encodeListKnown = findEncodings(images)
+print('encoding complete')
+
+cap = cv2.VideoCapture(0)
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS,faceCurFrame)
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        #print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            #print(name)
+            y1,x2,y2,x1=faceLoc
+            y1, x2, y2, x1= y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendence(name)
+
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        # print(lmList[4], lmList[8])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        # print(length)
+
+        # Hand range 50 - 300
+        # volume Range -65 - 0
+
+        vol = np.interp(length, [50, 210], [minVol, maxVol])
+        volBar = np.interp(length, [50, 300], [400, 150])
+        volPer = np.interp(length, [50, 300], [0, 100])
+        print(vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)}%', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
+
+
+
+
+
---
+++ READ_ME.txt
@@ -0,0 +1,38 @@
+To run the project,
+(PyCharm recommended)
+
+ please open Attendence_HandTrackingProject.py file,
+
+(this is the main project)
+
+----------------------------------------------------------------------------------------------------------------
+
+ in order to run, install (mediapipe, opencv, tensorflow, pycaw, comtypes, face_recognition) packages,
+
+ use PyCharm
+
+ in cmd use pip to install the packages or in PyCharm install the packages.
+
+-----------------------------------------------------------------------------------------------------------------
+
+The Attendence project has been given some personanlities to recognize, 
+
+those are (Elon Musk, Taylor Swift, Jeff Bezos, Bille Ellish, Jackie Chan, Bill Gates)
+use any of the above and run the project.
+
+to use these, have a another device and open any of the above personanities picture and hold it infront of your webcam/camera through which the program will recognize it.
+
+The Attendence.cvs file contains all the entries which were taken while the project was running.
+
+
+-------------------------------------------------------------------------------------------------------------------
+
+
+If you want to only run the HandTracking and Volume Control:
+
+open VolumeHandControl.py file
+
+click on your speaker volume at the bottom right corner, and use the gesture of your Thumb and Forefinger to increase or decrease the volume.
+(The Gesture is like pinching or reducing the space between fore finger and thumb).
+
+-------------------------------------------------------------------------------------------------------------------
